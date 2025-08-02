bc6a34152f19277ecf0594c03e126bea8e97cb78
Sebastian Schwaiger <sebastian.schwaiger@innoq.com>
2025-08-02 21:12:15 +0200

    Updated frontend code (added rainbow button)

--- web/dist/service-worker.js
+++ web/dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.022f501a0c2048a842b5014ef65792a0.js"
+  "/precache-manifest.4c77b927514bfa922af8a67ba1f224dd.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "adeept_bot_controller_web"});
